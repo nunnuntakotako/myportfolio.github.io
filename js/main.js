a226b5ab1d448ed88dd176e3ee4b9647557487eb
nozomi <x8ojo8x@gmail.com>
2023-02-12 21:00:57 +0900

    workpage renew

--- js/main.js
+++ js/main.js
@@ -1,6 +1,43 @@
 'use strict';
 
 {
+
+  function header(){
+    $.ajax({
+      url: "inc/header.html",
+      cache: false,
+      success: function(html){
+        document.write(html);
+      }
+    });
+  }
+
+  function side(){
+    $.ajax({
+      url: "inc/side.html",
+      cache: false,
+      success: function(html){
+        document.write(html);
+      }
+    });
+  }
+
+  function footer(){
+    $.ajax({
+      url: "inc/footer.html",
+      cache: false,
+      success: function(html){
+        document.write(html);
+      }
+    });
+  }
+
+
+}
+
+
+
+{
   const open = document.getElementById('open');
   const overlay = document.querySelector('.overlay');
   const close = document.getElementById('close');
